dccad963459f0b0037289233426e629ed61cbcd2
carterrm <carter.r.madsen@gmail.com>
2023-02-21 12:35:00 -0700

    Added update practice username & password

---
+++ python/update_query_strings.py
@@ -0,0 +1,3 @@
+update_practice_email = "UPDATE dental_practice SET email = \'{email}\' WHERE id = {id};"
+
+update_practice_password = "UPDATE dental_practice SET password = \'{hashed_pass}\' WHERE id = {id};"
