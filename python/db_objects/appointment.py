2e09c21bfa95a8d4fc3e72a295801952a09c7feb
carterrm <carter.r.madsen@gmail.com>
2023-02-27 12:15:04 -0700

    Resolve premature dependency on Appointments

---
+++ python/db_objects/appointment.py
@@ -0,0 +1,12 @@
+class Appointment:
+    def __init__(self, id, time, listed_price, full_price, filled, practice_id, patient_id, service_id, is_active):
+        Appointment.id = id
+        Appointment.time = time
+        Appointment.listed_price = listed_price
+        Appointment.full_price = full_price
+        Appointment.filled = filled
+        Appointment.practice_id = practice_id
+        Appointment.patient_id = patient_id
+        Appointment.service_id = service_id
+        Appointment.is_active = is_active
+
